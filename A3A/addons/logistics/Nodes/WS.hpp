1288b546f586628475d07600adb27c0cd60a8351
ante185 <49555217+ante185@users.noreply.github.com>
2023-10-07 20:52:44 +0200

    Adds logistics nodes for WS offroads (#2950)
    * Update WS.hpp
    
    * fixed height offset

--- A3A/addons/logistics/Nodes/WS.hpp
+++ A3A/addons/logistics/Nodes/WS.hpp
@@ -71,3 +71,37 @@ class lxws_vehicles_f_lxws_Truck_02_Truck_02_cargo_lxWS_p3d : TRIPLES(ADDON,Node
         };
     };
 };
+
+class lxws_vehicles_f_lxws_Offroad_01_Offroad_01_unarmed_lxWS_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {-0.05,-1.3,-0.683};
+            seats[] = {3,4};
+        };
+        class Node2
+        {
+            offset[] = {-0.05,-2.3,-0.683};
+            seats[] = {1,2};
+        };
+    };
+};
+
+class lxws_vehicles_f_lxws_Offroad_01_Offroad_01_armor_unarmed_lxWS_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+    class Nodes
+    {
+        class Node1
+        {
+            offset[] = {-0.035,-1.25,-1.135};
+            seats[] = {3,4};
+        };
+        class Node2
+        {
+            offset[] = {-0.035,-2.05,-1.135};
+            seats[] = {1,2};
+        };
+    };
+};
