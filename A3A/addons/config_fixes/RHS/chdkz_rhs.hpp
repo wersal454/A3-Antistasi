8730d26f44069475b47a1726bf106883759abbcc
ante <49555217+ante185@users.noreply.github.com>
2024-01-25 15:00:12 +0100

    Removed debug scope from a3a rhs t-72

--- A3A/addons/config_fixes/RHS/chdkz_rhs.hpp
+++ A3A/addons/config_fixes/RHS/chdkz_rhs.hpp
@@ -7,12 +7,6 @@ class rhsgref_ins_t72bc;
 
 class a3a_rhs_chdkz_72a : rhsgref_ins_t72ba
 {
-
-#if __A3_DEBUG__
-    scope = 2;
-#else
-    scope = 1;
-#endif
 	class EventHandlers
 	{
 		fired = "_this call (uinamespace getvariable 'BIS_fnc_effectFired');";
@@ -22,11 +16,6 @@ class a3a_rhs_chdkz_72a : rhsgref_ins_t72ba
 };
 class a3a_rhs_chdkz_72b : rhsgref_ins_t72bb
 {
-#if __A3_DEBUG__
-    scope = 2;
-#else
-    scope = 1;
-#endif
 	class EventHandlers
 	{
 		fired = "_this call (uinamespace getvariable 'BIS_fnc_effectFired');";
@@ -36,11 +25,6 @@ class a3a_rhs_chdkz_72b : rhsgref_ins_t72bb
 };
 class a3a_rhs_chdkz_72c : rhsgref_ins_t72bc
 {
-#if __A3_DEBUG__
-    scope = 2;
-#else
-    scope = 1;
-#endif
 	hiddenSelectionsTextures[] =
 	{
 		"rhsafrf\addons\rhs_t72_camo\data\rhs_t72b_01a_chdkz_co.paa",
