5fd735f1a327912a01cefcb7c33def3da04a96c0
wersal454 <wroma@bk.ru>
2024-05-11 22:53:42 +0300

    Update controls.hpp

--- A3A/addons/gui/dialogues/controls.hpp
+++ A3A/addons/gui/dialogues/controls.hpp
@@ -1072,6 +1072,11 @@ class A3A_DefaultDialog
     #define DIALOG_X CENTER_X(DIALOG_W) // Global x pos of dialog
     #define DIALOG_Y CENTER_Y(DIALOG_H) // Global y pos of dialog
 
+    #define DIALOG_VEHSTORE_W 320 // Width of dialog in pixelGrid units
+    #define DIALOG_VEHSTORE_H 200 // Height of dialog in pixelGrid units
+    #define DIALOG_VEHSTORE_X CENTER_X(DIALOG_W) // Global x pos of dialog
+    #define DIALOG_VEHSTORE_Y CENTER_Y(DIALOG_H) // Global y pos of dialog
+
     class ControlsBackground
     {
         class TitleBarBackground : A3A_Background
