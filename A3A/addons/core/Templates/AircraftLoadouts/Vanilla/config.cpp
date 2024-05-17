f343e5babfa6064c799a70e44303e9137b5aece2
ante <49555217+ante185@users.noreply.github.com>
2024-05-17 21:02:38 +0200

    Removed testing code

--- A3A/addons/core/Templates/AircraftLoadouts/Vanilla/config.cpp
+++ A3A/addons/core/Templates/AircraftLoadouts/Vanilla/config.cpp
@@ -38,11 +38,8 @@ class A3A {
                 mainGun[] = {"Twin_Cannon_20mm"};
                 rocketLauncher[] = {"Rocket_04_AP_Plane_CAS_01_F"};
                 missileLauncher[] = {"Missile_AGM_02_Plane_CAS_01_F", "missiles_SCALPEL"};
-                code = "params ['_plane']; [_plane,['Hex',1], true] call BIS_fnc_initVehicle;"; //example testing purposes
             };
-            class a3a_Plane_Fighter_03_grey_F : I_Plane_Fighter_03_dynamicLoadout_F{
-                code = "";
-            };
+            class a3a_Plane_Fighter_03_grey_F : I_Plane_Fighter_03_dynamicLoadout_F{};
         };
         class CAPPlane
         {
