c403cd506950d31c291aa8656318d6d867857f4b
SilenceIsFatto <78276788+SilenceIsFatto@users.noreply.github.com>
2024-06-02 21:46:36 +0100

    Update vehicles_tmt.hpp

--- A3A/addons/ultimate/config/trader/vehicles/vehicles_tmt.hpp
+++ A3A/addons/ultimate/config/trader/vehicles/vehicles_tmt.hpp
@@ -3,5 +3,13 @@ class vehicles_tmt : vehicles_base
     ITEM(TMT_Landrover_w_AA, 41000, "AA", "true");
     ITEM(TMT_ACV300_W_Amb, 6320, "APC", "true");
     ITEM(TMT_ACV300_W_M2, 5940, "APC", "true");
+    ITEM(TMT_KirpiII_MRAP_W, 13300, "ARMEDCAR", "true");
+    ITEM(TMT_Cobra_RCWS_W, 9550, "ARMEDCAR", "true");
+    ITEM(TMT_Ejder_RCWS_GEN, 12550, "ARMEDCAR", "true");
+    ITEM(TMT_Arma_GMG_HMG, 18850, "ARMEDCAR", "true");
     ITEM(TMT_T155_W, 55800, "ARTILLERY", "true");
+    ITEM(TMT_LEO2A4_w, 37100, "TANK", "true");
+    ITEM(TMT_Leopard2_NG_W, 46900, "TANK", "true");
+    ITEM(TMT_BayraktarTB2, 23000, "UAV", "true");
+    ITEM(TMT_Kirpi_MRAP_GEN, 2040, "UNARMEDCAR", "true");
 };
