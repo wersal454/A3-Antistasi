557044e1028e9c499a70f45abbe9b14c2da097c5
stutpip123 <77023662+stutpip123@users.noreply.github.com>
2024-06-12 18:13:30 +0100

    Reb and Riv

--- A3A/addons/core/Templates/Templates/CWR/templates.hpp
+++ A3A/addons/core/Templates/Templates/CWR/templates.hpp
@@ -24,3 +24,27 @@
         file = "CWR_AI_US_Army_Arid";
 		climate[] = {"arid"};
     };
+
+    class CWR_USSR_Temperate : CWR_Base
+    {
+        side = "Inv";
+        flagTexture = "cwr3\general\cwr3_flags\data\ussr.paa";
+        name = "CWR USSR Temperate";
+        file = "CWR_AI_USSR_Temperate";
+    };
+	
+    class CWR_Reb : CWR_Base
+    {
+        side = "Reb";
+        flagTexture = "cwr3\general\cwr3_flags\data\fia.paa";
+        name = "CWR FIA";
+        file = "CWR_Reb_FIA";
+    };
+
+    class CWR_ComMil : CWR_Base
+    {
+        side = "Riv";
+        flagTexture = "cwr3\general\cwr3_flags\data\belarus_old.paa";
+        name = "CWR Communist Militia";
+        file = "CUP_Riv_ComMilitia";
+    };
