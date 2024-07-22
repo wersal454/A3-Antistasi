c6508deac9b164b0ae873529bc5413f28c9c87c8
wersal454 <wroma@bk.ru>
2024-07-22 04:37:44 +0300

    part 1 ?

---
+++ A3A/addons/ultimate/config/trader/vehicles/vehicles_csla.hpp
@@ -0,0 +1,132 @@
+/* class vehicles_gm : vehicles_base
+{
+	CSLA_PLdvK59V3S_noinsignia aa
+	CSLA_BVP1_noinsignia apc
+	CSLA_BPzV_noinsignia apc
+	CSLA_DTP90_noinsignia repair(apc)
+	CSLA_MU90_noinsignia minelayer(apc)
+	CSLA_OT62_noinsignia apc
+	CSLA_OT64C_noinsignia apc
+	CSLA_OT65A_noinsignia apc
+	CSLA_OZV90_noinsignia medical(apc)
+	CSLA_RM51_noinsignia arty
+	CSLA_RM70_noinsignia arty
+	CSLA_ShKH77_noinsignia arty
+	CSLA_AZU_noinsignia car
+	CSLA_AZU_para_noinsignia car
+	CSLA_AZU_R2_noinsignia car
+	CSLA_CS_CATOR_noinsignia car
+	CSLA_F813o_noinsignia truck
+	CSLA_F813_noinsignia truck
+	CSLA_V3SLizard_noinsignia truck
+	CSLA_V3Sa_noinsignia truck (ammo)
+	CSLA_V3Sf_noinsignia truck (fuel)
+	CSLA_V3So_noinsignia truck
+	CSLA_V3S_noinsignia truck
+	CSLA_V3Sr_noinsignia truck (repair)
+	CSLA_Mi17_noinsignia heli
+	CSLA_Mi17mg_noinsignia heli
+	CSLA_Mi17pylons_noinsignia heli
+	CSLA_Mi24V_noinsignia heli
+	CSLA_T72_noinsignia tank
+	CSLA_T72M_noinsignia tank
+	CSLA_T72M1_noinsignia tank
+	FIA_BTR40_noinsignia apc(more like a car)
+	FIA_BTR40_DSKM_noinsignia apc(more like a car)
+	FIA_AZU_para_noinsignia car
+	FIA_AZU_DSKM_noinsignia armedcar
+	FIA_AZU_T21_noinsignia car
+	CSLA_FIA_CATOR_noinsignia car
+	FIA_OT64C_VB apc
+	CSLA_PLdvK59_Stat staticaa
+	CSLA_T21_Stat staticat
+
+	AFMC_M163 aa
+	AFMC_LAV25 apc
+	AFMC_M113A2_AMB medical(apc)
+	AFMC_M113A2_DTP repair(apc)
+	AFMC_M113A1 apc
+	AFMC_M113A2ext apc
+	AFMC_M270 arty
+	AFMC_M1008c car
+	AFMC_M1008 car
+	AFMC_M1008_S250 car
+	AFMC_M923a truck(ammo)
+	AFMC_M923cargo truck
+	AFMC_M923f truck(fuel)
+	AFMC_M923r truck(repair)
+	AFMC_M923o truck
+	AFMC_M923c truck
+	AFMC_AH1F heli
+	AFMC_UH60 heli
+	AFMC_UH60M240 heli
+	AFMC_M1A1 tank
+	AFMC_M1IP tank
+	AFMC_M252_Stat mortar
+	AFMC_M2h hmg
+	AFMC_M2l hmg
+	AFMC_TOW_Stat staticat
+
+	US85_M163 aa
+	US85_LAV25 apc
+	US85_M113_AMB apc (medical)
+	US85_M113_DTP apc (repair)
+	US85_M113 apc
+	US85_M270 arty
+
+	US85_zodiac boat
+	CSLA_lodka boat
+
+	US85_M1008c car
+	US85_M1008 car
+	US85_M1008_S250 car
+	US85_M1025_ua car
+	US85_M1025_M2 armedcar 
+	US85_M1025_M60 armedcar
+	US85_M1030 car
+	US85_M1043_ua car
+	US85_M1043_M2 armedcar
+	US85_M1043_M60 armedcar
+	US85_M923a truck(ammo)
+	US85_M923cargo truck
+	US85_M923f truck(fuel)
+	US85_M923r truck(repair)
+	US85_M923o truck
+	US85_M923c truck
+	US85_M923a1_a truck(ammo)
+	US85_M923a1_cargo truck
+	US85_M923a1_f truck(fuel)
+	US85_M923a1_r truck(repair)
+	US85_M923a1_s280 truck
+	US85_M923a1o truck
+	US85_M923a1om2 truck
+	US85_M923a1c truck
+	US85_M923a1cm2 truck
+	US85_M998SFGT armedcar
+	US85_AH1F heli
+	US85_MH60M134 heli
+	US85_MH60FFAR heli
+	US85_UH60 heli
+	US85_UH60M240 heli
+	US85_M1A1 tank
+	US85_M1IP tank
+	US85_M2h hmg
+	US85_M2l hmg
+	US85_TOW_Stat staticat
+
+	CSLA_CIV_An2_1 plane
+
+    ITEM(gm_ge_army_gepard1a1_noinsignia, 19500, "AA", VEHICLE_CONDITION_AA);
+    ITEM(gm_pl_army_ot64a, 7112, "APC", VEHICLE_CONDITION_APC);
+    ITEM(gm_dk_army_u1300l_container, 3600, "ARMEDCAR", VEHICLE_CONDITION_ARMEDCAR);
+    ITEM(gm_pl_army_ural375d_mlrs, 60390, "ARTILLERY", VEHICLE_CONDITION_ARTILLERY);
+    ITEM(gm_pl_airforce_mi2urpg_noinsignia, 20225, "HELI", VEHICLE_CONDITION_HELI);
+    ITEM(gm_ge_airforce_do28d2_noinsignia, 16600, "PLANE", VEHICLE_CONDITION_PLANE);
+    ITEM(gm_gc_bgs_searchlight_01, 1000, "STATICAA", VEHICLE_CONDITION_STATICAA);
+    ITEM(gm_ge_army_milan_launcher_tripod, 6750, "STATICAT", VEHICLE_CONDITION_STATICAT);
+    ITEM(gm_gc_army_dshkm_aatripod, 3600, "STATICMG", VEHICLE_CONDITION_STATICMG);
+    ITEM(gm_dk_army_bpz2a0_noinsignia, 17000, "TANK", VEHICLE_CONDITION_TANK);
+    ITEM(gm_gc_army_t55am2b_noinsignia, 46500, "TANK", VEHICLE_CONDITION_TANK);
+    ITEM(gm_ge_army_u1300l_medic, 7900, "UNARMEDCAR", VEHICLE_CONDITION_UNARMEDCAR);
+};
+ */
